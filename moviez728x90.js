e1e2cdbd0caea015df20bcbf10049f073f8b5bad
movie <145183125+webmovi@users.noreply.github.com>
2023-09-16 17:22:12 +0700

    Create moviez728x90.js

---
+++ moviez728x90.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '58430bc5b8498fc0226fd564cebdfb8c',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/58430bc5b8498fc0226fd564cebdfb8c/invoke.js"></scr' + 'ipt>');
